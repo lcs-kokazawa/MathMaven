2a68def031fce06ca24550bec9233dd1c40e424f
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-05-28 16:01:57 -0400

    Made a stored properties and computed properties.

--- MathMaven/Views/MultiplicationView.swift
+++ MathMaven/Views/MultiplicationView.swift
@@ -8,10 +8,65 @@
 import SwiftUI
 
 struct MultiplicationView: View {
+    
+    //MARK: Stoored properties
+    
+    // The numbers to be multiplied
+    @State var firstValue = Int.random(in: 1...72)
+    @State var secondValue = Int.random(in: 1...72)
+    
+    // Holds whatever input was provided by the user
+    @State var input = ""
+    
+    // Has the user's answer been checked?
+    @State var answerChecked = false
+    
+    // Was the user's given answer correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    
+    //The correct response
+    var correctResponse: Int {
+        return firstValue * secondValue
+    }
+    
     var body: some View {
-        VStack {
-            Text("Multiplication")
-            Text(Operation.multiplication.rawValue)
+        VStack(spacing: 0) {
+            
+            // 1.Present question
+            Group {
+                HStack {
+                    Text(Operation.multiplication.rawValue)
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("\(firstValue)")
+                        Text("\(secondValue)")
+                        
+                        
+                    }
+                }
+                Divider()
+            }
+            .padding(.horizontal)
+            
+            // 2. Accept answer
+            HStack {
+                
+                ZStack {
+                    //Only show this when the answer was found to be correct
+                    if answerCorrect == true {
+                        Image(systemName: "checkmark.circle")
+                            .foregroundColor(.green)
+                    }
+                    
+                    //Show this when the answer was
+                }
+            }
+            
+            
         }
         .font(Font.custom("SF Pro", size: 64))
     }
